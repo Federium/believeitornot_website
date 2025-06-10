cec320a02d960f4e91d3aaf7056b45dca4a903ed
Filippo Mirandola <filippo.mirandola@outlook.com>
2025-06-10 09:31:31 +0200

    aggiunti team

--- src/stores/testi.js
+++ src/stores/testi.js
@@ -1,26 +1,26 @@
 export const mapTesti = {
   nwo: {
     whatif: {
-      it: 'E se il nwo esistesse?',
-      en: 'What if the nwo existed'
+      it: 'E se  il dominio globale iniziasse con l’eliminazione delle parole?',
+      en: 'What if global supremacy starts by eliminating words?'
     },
     scenario: {
       it: `
-La teoria del Progetto Blue Beam sostiene l’esistenza di un piano segreto per instaurare un Nuovo Ordine Mondiale. Secondo questa teoria, attraverso l’uso di tecnologie avanzate si simulerebbero eventi soprannaturali, come apparizioni divine o invasioni aliene, per generare un caos tale da giustificare, agli occhi della popolazione, l’implementazione di un governo mondiale centralizzato.<br>
+La teoria del Progetto Blue Beam sostiene l’esistenza di un piano segreto per instaurare un Nuovo Ordine Mondiale. Secondo questa teoria, attraverso l’uso di tecnologie avanzate si simulerebbero eventi soprannaturali, come apparizioni divine o invasioni aliene, per generare un caos tale da giustificare, agli occhi della popolazione, l’implementazione di un governo mondiale centralizzato.
 Il piano si basa sulla manipolazione delle percezioni umane, unita al controllo delle tecnologie di comunicazione e dei sistemi psicologici, per assicurare l’unificazione globale sotto un unico potere.
       `,
       en: `
-The Blue Beam Project theory claims the existence of a secret plan to establish a New World Order. According to this theory, through the use of advanced technologies, supernatural events such as divine apparitions or alien invasions would be simulated to create chaos and justify, in the eyes of the population, the implementation of a centralized world government.<br>
+The Blue Beam Project theory claims the existence of a secret plan to establish a New World Order. According to this theory, through the use of advanced technologies, supernatural events such as divine apparitions or alien invasions would be simulated to create chaos and justify, in the eyes of the population, the implementation of a centralized world government.
 The plan relies on manipulating human perception, alongside control of communication technologies and psychological systems, to ensure global unification under a single power.
       `
     },
     progetto: {
       it: `
-In un mondo sempre più frammentato da diverse lingue e culture, Lingua si presenta come soluzione al disordine globale: un linguaggio sintetico progettato non per esprimere il pensiero, ma per controllarlo. Parte integrante del più ampio piano Blue Beam, che prevede l’ascesa di un governo mondiale centralizzato, Lingua riduce l’intera comunicazione a 55 simboli universali, ognuno associato a specifiche frequenze vibrazionali e gesti manuali. <br>
+In un mondo sempre più frammentato da diverse lingue e culture, Lingua si presenta come soluzione al disordine globale: un linguaggio sintetico progettato non per esprimere il pensiero, ma per controllarlo. Parte integrante del più ampio piano Blue Beam, che prevede l’ascesa di un governo mondiale centralizzato, Lingua riduce l’intera comunicazione a 55 simboli universali, ognuno associato a specifiche frequenze vibrazionali e gesti manuali.
 Essendo uno strumento per la pace e il progresso, Lingua elimina l’ambiguità e la complessità standardizzando il pensiero stesso. Ciò che sembra una conquista è in realtà un sistema di controllo – dove è il linguaggio a plasmare le persone, anziché le persone plasmare il linguaggio.
       `,
       en: `
-In a world increasingly fragmented by different languages and cultures, Lingua is presented as a solution to global disorder: a synthetic language designed not to express thought, but to control it. As part of the broader Blue Beam plan to establish a centralized world government, Lingua reduces all communication to 55 universal symbols, each linked to specific vibrational frequencies and hand gestures.<br>
+In a world increasingly fragmented by different languages and cultures, Lingua is presented as a solution to global disorder: a synthetic language designed not to express thought, but to control it. As part of the broader Blue Beam plan to establish a centralized world government, Lingua reduces all communication to 55 universal symbols, each linked to specific vibrational frequencies and hand gestures.
 As a tool for peace and progress, Lingua eliminates ambiguity and complexity by standardizing thought itself. What seems like an achievement is in fact a control system — where language shapes people, instead of people shaping language.
       `
     },
@@ -73,7 +73,7 @@ A hierarchical structure does not characterise Sneic; it is divided into three s
 
   milestone: {
     whatif: {
-      it: 'l’essere umano non sia mai stato sulla Luna?',
+      it: 'E se l’essere umano non fosse mai stato sulla Luna?',
       en: 'What if humans has never been on the Moon?'
     },
     scenario: { it: `Nel 1969 la NASA afferma che l’uomo aveva messo piede sulla Luna durante la missione Apollo 11, segnando un momento fondamentale nella storia dell'esplorazione spaziale.
